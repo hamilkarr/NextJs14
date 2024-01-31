7917df2d21c522e6363c120ac8e66b13434c7804
hamilkarr <12uclock@gmail.com>
2024-02-01 04:49:12 +0900

    네비게이션 바 추가

---
+++ app/about-us/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../../components/navigation";
+
+export default function AboutUsPage() {
+    return (
+        <div>
+            <Navigation />
+            <h1>About Us!</h1>
+        </div>
+    );
+}
