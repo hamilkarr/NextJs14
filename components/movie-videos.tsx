7d94dc68a4812b89c9eb2488717db862fd4f131c
hamilkarr <12uclock@gmail.com>
2024-02-07 01:13:20 +0900

    메인 css 완료

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,7 +2,6 @@ import { API_URL } from '../app/(home)/page';
 
 async function getVideos(id: string) {
     await new Promise((resolve) => setTimeout(resolve, 3000));
-    throw new Error('Failed to fetch videos');
     // const response = await fetch(API_URL + `/${id}/videos`);
     // return response.json();
 }
