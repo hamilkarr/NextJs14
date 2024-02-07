4f164c3b49c79c53ab646ccdcb8aa4c68ac715ab
hamilkarr <12uclock@gmail.com>
2024-02-08 00:23:30 +0900

    피로회복

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,12 +1,24 @@
 import { API_URL } from '../app/(home)/page';
+import styles from './movie-videos.module.css';
 
 async function getVideos(id: string) {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    // const response = await fetch(API_URL + `/${id}/videos`);
-    // return response.json();
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    const response = await fetch(API_URL + `/${id}/videos`);
+    return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
     const videos = await getVideos(id);
-    return <h6>{JSON.stringify(videos)}</h6>;
+    return (
+        <div className={styles.container}>
+            {videos.map((video) => (
+                <iframe
+                    key={video.id}
+                    src={`https://www.youtube.com/embed/${video.key}`}
+                    allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                    title={video.name}
+                />
+            ))}
+        </div>
+    );
 }
